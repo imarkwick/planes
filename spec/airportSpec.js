79e35450dd6bdd25de6351ad7d27dc08d1e89743
imarkwick <izzymarkwick@gmail.com>
2014-12-07 21:52:22 +0000

    a few more tests

---
+++ spec/airportSpec.js
@@ -0,0 +1,31 @@
+describe ('Airport', function() {
+
+	var airport, plane;
+
+	beforeEach(function () {
+		london = new Airport('london');
+		airport = new Airport('name');
+		plane = new Plane;
+	});
+
+	it('should have a name', function() {
+		expect(london.name).toEqual('london')
+	});
+
+	it('should be able to count the number of parked planes', function() {
+		expect(airport.planeCount()).toEqual(0)
+	});
+
+	it('should let planes land', function() {
+		airport.parks(plane)
+		expect(airport.planeCount()).toEqual(1)
+	});
+
+	it('should let planes take off', function() {
+		airport.parks(plane)
+		expect(airport.planeCount()).toEqual(1)
+		airport.releases(plane)
+		expect(airport.planeCount()).toEqual(0)
+	});
+
+});
--- spec/planeSpec.js
+++ spec/planeSpec.js
@@ -7,14 +7,14 @@ describe('Plane', function() {
 		airport = new Airport();
 	});
 
-		it('should land at the airport', function() {
-			plane.lands(airport);
-			expect(plane.flying).toBe(false);
-		});
+	it('should land at the airport', function() {
+		plane.lands(airport);
+		expect(plane.flying).toBe(false);
+	});
 
-		it('should take off from the airport', function() {
-			plane.takeOff(airport);
-			expect(plane.flying).toBe(true);
-		});
+	it('should take off from the airport', function() {
+		plane.takeOff(airport);
+		expect(plane.flying).toBe(true);
+	});
 
 });
