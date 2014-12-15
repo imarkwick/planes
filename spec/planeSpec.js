049834904997afab7706fc5b1cb4dc42eb92a7c7
imarkwick <izzymarkwick@gmail.com>
2014-12-15 15:07:03 +0000

    jasmine doubles and extra tests

--- spec/planeSpec.js
+++ spec/planeSpec.js
@@ -1,29 +1,43 @@
 describe('Plane', function() {
 
-	var plane, airport;
+	var plane;
 
 	beforeEach(function() {
 		plane = new Plane();
-		airport = new Airport();
+		airport = jasmine.createSpyObj('airport', ['parks', 'releases']);
 	});
 
-	it('should land at the airport', function() {
-		plane.lands(airport);
-		expect(plane.flying).toBe(false);
+	it('is flying when initialized', function() {
+		expect(plane.isFlying()).toBe(true);
+	});
+
+	it('should be able to land', function() {
+		plane.land();
+		expect(plane.isFlying()).toBe(false);
+	});
+
+	it('should be able to land at an airport', function() {
+		plane.landOn(airport);
+		expect(airport.parks).toHaveBeenCalledWith(plane);
+	});
+
+	it('is not flying after landing', function() {
+		plane.landOn(airport);
+		expect(plane.isFlying()).toBe(false);
 	});
 
 	it('should take off from the airport', function() {
-		plane.takeOff(airport);
-		expect(plane.flying).toBe(true);
+		plane.takeOffFrom(airport);
+		expect(airport.releases).toHaveBeenCalledWith(plane);
 	});
 
 	it('should be at the airport once landed', function() {
-		plane.lands(airport);
-		expect(airport.planes).toEqual([plane]);
+		plane.landOn(airport);
+		expect(airport.parks).toHaveBeenCalledWith(plane);
 	});
 
-	it('should be out of the airport when flying', function() {
-		plane.takeOff(airport);
-		expect(airport.planes).toEqual([]);
+	it('is flying after takeoff', function() {
+		plane.takeOffFrom(airport);
+		expect(plane.flying).toBe(true);
 	});
 });
